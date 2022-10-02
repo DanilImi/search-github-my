1f540c1227d9acc06af727234b1438704234847b
Danil Razumov <dumirov03@mail.ru>
2022-10-02 18:49:17 +0300

    Refactor:  change dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redusers/index1';
 import DataRoutes from './DataRoutes';
@@ -13,4 +11,3 @@ root.render(
 	</Provider>
 );
 
-reportWebVitals();
